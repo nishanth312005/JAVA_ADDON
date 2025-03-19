cc694e2a93f8a98e1b453499b18676561b67a9c8
nishanth312005 <nishanthmanian31305@gmail.com>
2025-03-19 21:57:41 +0530

    day2

---
+++ day2/String_Builder.java
@@ -0,0 +1,40 @@
+//Strings are immutable
+
+//String builder(not safe) and buffer(thread safe) are mutable
+
+package day2;
+
+import java.util.*;
+
+public class String_Builder {
+
+	public static void main(String[] args) {
+
+		StringBuilder sb=new StringBuilder("Hello");
+
+		//append
+
+		sb.append(" World");
+
+		System.out.println("String Append:" + sb);
+
+		//insert
+
+		sb.insert(5,",");
+
+		System.out.println("String Insert:" + sb);
+
+		//replace
+
+		sb.replace(0, 5, "Welcome");
+
+		System.out.println("String replace:" + sb);
+
+		//reverse
+
+		sb.reverse();
+
+		System.out.println("String reverse:" + sb);
+
+		}
+}
