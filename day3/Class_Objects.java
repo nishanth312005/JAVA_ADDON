30be605b760bbbe59474339b2d96968032b74a57
nishanth312005 <nishanthmanian31305@gmail.com>
2025-03-19 22:06:59 +0530

    day3

---
+++ day3/Class_Objects.java
@@ -0,0 +1,28 @@
+package day3;
+import java.util.*;
+
+class Student{
+	String name,dept;
+	int rno;
+	
+	void display() {                 // method creation
+		System.out.println(name);
+		System.out.println(rno);
+		System.out.println(dept);
+		
+	}
+}
+public class Class_Objects {
+	public static void main (String [] args) {
+		Student s = new Student();   //Object creation
+		s.name = "Nishanth";
+		s.rno = 223222;
+		s.dept = "Software Systems";
+		
+		/*System.out.println(s.name);
+		System.out.println(s.rno);
+		System.out.println(s.dept);	*/
+		
+		s.display();
+	}
+}
